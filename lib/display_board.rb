6822fd379a18dd9f171f8539229c5c84104aac84
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-01-30 20:27:36 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,19 +1,8 @@
-def display_board(the_board)
-  puts " #{the_board[0]} | #{the_board[1]} | #{the_board[2]}  "
+def display_board
+  puts "   |   |   " 
   puts "-----------"
-  puts " #{the_board[3]} | #{the_board[4]} |  #{the_board[5]} "
+  puts "   |   |   "
   puts "-----------"
-  puts " #{the_board[6]} | #{the_board[7]} |  #{the_board[8]} "
+  puts "   |   |   "
 end
 
-puts " A Tic Tac Toe Board"
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board(board)
-
-puts "Turn 1"
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-display_board(board)
-
-puts "Turn 2"
-board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
-display_board(board)
